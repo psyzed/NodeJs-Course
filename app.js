d16c2ae6a4ea6dbd9fa99920a5cbac069abb21b7
psyzed <psyzed66@gmail.com>
2026-02-10 20:32:35 +0200

    added ejs as default view engine

--- app.js
+++ app.js
@@ -3,22 +3,12 @@ const bodyParser = require("body-parser");
 const app = express();
 const rootDir = require("./utils/path");
 const path = require("path");
-const handlebars = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-// app.engine('pug', require('pug').__express)
-// app.set("view engine", "pug");
-app.engine(
-  "hbs",
-  handlebars({
-    layoutDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  }),
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
+app.engine("ejs", require("ejs").__express);
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { docTitle: "Page Not Found!" });
+  res.status(404).render("404", { docTitle: "Page Not Found!", path: "/404" });
 });
 
 app.listen(3000);
